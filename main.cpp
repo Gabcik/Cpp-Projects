cd7c0b722c25c20b4f915118d3ab51415d1dcd77
Gabcik <gab563riela@gmail.com>
2022-04-17 23:49:06 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+char getBoard ()
+{
+    cout << "| " << field1 << " || " << field2 << " || " <<
+         << " ---  ---  ---"
+         << "| " << field1 << " || " << field2 << " |"
+}
+
+int main() {
+    cout << "ONE-ARMED BANDIT" << endl
+    << "Pay 50 zl to play." << endl;
+
+    int declarationToPlay;
+    cin >> declarationToPlay;
+    return 0;
+}
