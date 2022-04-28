807a9ba865b9c2c40a373d86ebbd231d9bee2647
Gabcik <gab563riela@gmail.com>
2022-04-28 12:11:36 +0200

    count arithmetic mean

--- chapter-10/task-1/main.cpp
+++ chapter-10/task-1/main.cpp
@@ -2,21 +2,33 @@
 #include <string>
 using namespace std;
 
+float arithmeticMean (float sumOfNumbers);
+
+float fiveNumbers [5];
 
 int main()
 {
     cout << "I count the arithmetic mean of 5 numbers. Enter your numbers: " << endl;
-    int fiveNumbers [5];
-    cin >> fiveNumbers [0];
-    cin >> fiveNumbers [1];
-    cin >> fiveNumbers [2];
-    cin >> fiveNumbers [3];
-    cin >> fiveNumbers [4];
+
+    float sumOfNumbers;
+    for (int i = 0; i<5; i++)
+    {
+        cin >> fiveNumbers[i];
+        sumOfNumbers+=fiveNumbers[i];
+    }
 
     cout << "Your arithmetic mean is:" << endl
     << "(" << fiveNumbers [0] << "+" << fiveNumbers [1] << "+" << fiveNumbers [2] << "+" << fiveNumbers [3] << "+" << fiveNumbers [4] << ")"
-    << "/" << 5 << "=";
+    << "/" << 5 << "=" << arithmeticMean(sumOfNumbers);
+
+//    cout << endl << "give me all values from array" << fiveNumbers [5];
 
     //    getline (cin, fiveNumbers [5]);
     return 0;
+}
+
+float arithmeticMean (float sumOfNumbers)
+{
+    float arithmeticMean = sumOfNumbers/5;
+    return arithmeticMean;
 }
