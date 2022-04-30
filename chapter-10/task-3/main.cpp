3b75f7d68285a0e5873423cd7c0f1a7bae85312e
Gabcik <gab563riela@gmail.com>
2022-04-30 19:34:42 +0200

    added function

--- chapter-10/task-3/main.cpp
+++ chapter-10/task-3/main.cpp
@@ -2,22 +2,27 @@
 #include <string>
 using namespace std;
 
+void getNumbers (int table[][10]);
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, below you can see multiplication Table: " << endl;
     int multiplicationTable [10][10] = { {1,2,3,4,5,6,7,8,9,10},
                            {2}, {3}, {4}, {5}, {6 }, {7}, {8}, {9}, {10}};
 
+    getNumbers(multiplicationTable);
+
+    return 0;
+}
+
+void getNumbers (int table[][10])
+{
     for (int i=1; i<10; i++)
     {
         for (int j=0; j<10; j++)
         {
-            multiplicationTable [i][j] = multiplicationTable[i][0]*multiplicationTable[0][j];
-            cout << multiplicationTable[i][0] << "*"<< multiplicationTable[0][j] << "=" <<  multiplicationTable[i][j] << endl;
+            table [i][j] = table[i][0]*table[0][j];
+            cout << table[i][0] << "*"<< table[0][j] << "=" <<  table[i][j] << endl;
         }
     }
-
-    return 0;
-
 }
-
