e19fd990f298cb8b884223897e07ac044e7da807
Gabcik <gab563riela@gmail.com>
2022-05-03 16:35:26 +0200

    added check if sorted function

--- chapter-10/task-6/main.cpp
+++ chapter-10/task-6/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <string>
-#include <random>
 using namespace std;
 
+string checkIfSorted (int arrayOfNumbers[]);
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int numbers [] = {1,2,3,4,5,11,7,8,9,10};
+    string answerIfSorted = checkIfSorted(numbers);
+    cout << "The numbers are sorted: " << answerIfSorted << endl;
     return 0;
+}
+
+string checkIfSorted (int arrayOfNumbers[]) {
+    string response;
+    for (int i = 0; i < 9; i++) {
+        if (arrayOfNumbers[i] < arrayOfNumbers[i + 1]) {
+            response = "true";
+        }
+        else {
+            response = "false";
+            break;
+        }
+    }
+
+    return response;
 }
